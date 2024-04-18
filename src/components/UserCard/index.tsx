a00af76b2a5df66032ab5576df2db47f4f7f5eac
kukuha <kukuha@poehala.kuda>
2024-04-18 11:16:58 +0300

    add userCard component

---
+++ src/components/UserCard/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./UserCard.module.scss";
+import Avatar from "../Avatar";
+import ExitIcon from "../../svg/ExitIcon";
+
+interface UserCardProps {
+  user: { title: string; src: string };
+}
+
+const defaultProps: UserCardProps = {
+  user: { title: "Username", src: "" },
+};
+
+function UserCard({ user }: UserCardProps): JSX.Element {
+  return (
+    <div className={styles.userCardBlock}>
+      <div className={styles.leftPart}>
+        <Avatar
+          isOnline={false}
+          alt={user.title}
+          isOutline={false}
+          width={60}
+          key="im-user"
+          src={user.src}
+        />
+        <div className={styles.userCardContent}>
+          <span>Привет,</span>
+          <span>{user.title}</span>
+        </div>
+      </div>
+
+      <button className={styles.exit}>
+        <ExitIcon width={18} height={16} />
+      </button>
+    </div>
+  );
+}
+UserCard.defaultProps = defaultProps;
+
+export default UserCard;
