fe6a345a1241b2f9173e376e4a9f33a9940d27de
Саня <70769398+Deadend4@users.noreply.github.com>
2024-05-13 15:20:17 +0300

    add settings icons

---
+++ src/utils/getSettingsIcon.tsx
@@ -0,0 +1,36 @@
+import TimeZone from "../../assets/settingsIcon/TimeZone.png";
+import Language from "../../assets/settingsIcon/Language.png";
+import DarkMode from "../../assets/settingsIcon/DarkModeIcon.png";
+import EmailNotifications from "../../assets/settingsIcon/EmailNotifications.png";
+import AppNotifications from "../../assets/settingsIcon/AppNotifications.png";
+import LocationAccess from "../../assets/settingsIcon/LocationAccess.png";
+import PhotoAccess from "../../assets/settingsIcon/PhotoAccess.png";
+
+type settingsIconType =
+  | "timezone"
+  | "language"
+  | "darkMode"
+  | "emailNotifications"
+  | "appNotifications"
+  | "locationAccess"
+  | "photoAccess";
+export default function getSettingsIcon(settingIcon: settingsIconType) {
+  switch (settingIcon) {
+    case "timezone":
+      return TimeZone;
+    case "language":
+      return Language;
+    case "darkMode":
+      return DarkMode;
+    case "emailNotifications":
+      return EmailNotifications;
+    case "appNotifications":
+      return AppNotifications;
+    case "locationAccess":
+      return LocationAccess;
+    case "photoAccess":
+      return PhotoAccess;
+    default:
+      return "/assets/placeholder.png";
+  }
+}
